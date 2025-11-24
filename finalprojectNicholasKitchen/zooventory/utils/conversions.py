5b565769e693a6dc83e34dfcd3271ea58edb438c
Nicholas Kitchen <njkitchen@mail.fhsu.edu>
2025-11-24 14:53:09 -0600

    Created conversions.py utility to help convert weight and volume amounts to grams and milliliters, respectively.

---
+++ finalprojectNicholasKitchen/zooventory/utils/conversions.py
@@ -0,0 +1,25 @@
+# Weight conversions to grams
+GRAM_CONVERSION = {
+    'g': 1,
+    'oz': 28.3495,
+    'lb': 453.592,
+}
+
+# Volume conversions to milliliters
+VOLUME_CONVERSION = {
+    'fl oz': 29.5735,
+    'gal': 3785.41,
+    'l': 1000,
+}
+
+# Convert weight to grams. Return none if unit is unknown
+def convert_to_grams(amount, unit):
+    if unit in GRAM_CONVERSION:
+        return amount * GRAM_CONVERSION[unit]
+    return None
+
+# Convert volume to milliliters. Return none if unit is unknown
+def convert_to_volume(amount, unit):
+    if unit in VOLUME_CONVERSION:
+        return amount * VOLUME_CONVERSION[unit]
+    return None
